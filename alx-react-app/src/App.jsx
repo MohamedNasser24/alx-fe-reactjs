dbae2e0148bf031f32e9f57de1bffa02b14229d3
MohamedNasser24 <Mohamednasser9354@gmail.com>
2024-08-07 08:27:51 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';  // Import statement for WelcomeMessage
 import UserProfile from './components/UserProfile';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <WelcomeMessage />  {/* Usage of WelcomeMessage component */}
             <MainContent />
             <UserProfile
                 name="Alice"
