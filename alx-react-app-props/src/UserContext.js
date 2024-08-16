07a4ef18e73a16b27b3c4e5c29ef45854f45f0d0
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-16 23:48:27 +0300

    git commit

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import UserInfo from './UserInfo';
+import UserDetails from './UserDetails';
 
-function ProfilePage() {
-  return <UserInfo />;
+function UserInfo() {
+  return <UserDetails />;
+}
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserDetails;
+export default UserInfo;
