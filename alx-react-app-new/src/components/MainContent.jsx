f2034551c2402603f130fa8702b7430eac19f5f4
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-16 21:54:58 +0300

    git commit

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
-      <h2>Welcome to the Main Content</h2>
-      <p>This is where the main content goes.</p>
+      <h2 style={{ color: 'darkblue', fontSize: '24px' }}>Welcome to the Main Content</h2>
+      <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
