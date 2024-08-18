7cf7c077dbaaaa1dffa357c3422502c5b4741d18
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-18 21:58:54 +0300

    git commit

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,8 +1,15 @@
-// src/UserContext.js
 import React, { createContext } from 'react';
 
 // Create a Context for user data
 const UserContext = createContext();
 
-// Export the Context and its Provider component
-export { UserContext };
+// Create a Provider component
+export const UserProvider = ({ children, userData }) => {
+  return (
+    <UserContext.Provider value={userData}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
