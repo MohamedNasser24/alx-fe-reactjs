58592857871ca0b60230ca52f22d32e7b7ae2427
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-18 22:58:55 +0300

    git commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ProfilePage from '../ProfilePage';
-import { UserProvider } from './UserContext'; // Import UserProvider
+import ProfilePage from './ProfilePage';
+import { UserProvider } from './components/UserContext'; // Import UserProvider
 
 function App() {
-  // Define the user data to be provided through context
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserProvider userData={userData}> {/* Wrap ProfilePage with UserProvider */}
-      <ProfilePage />
+      <ProfilePage /> {/* Main component */}
     </UserProvider>
   );
 }
