7b44b65c72b060c18f7b5bf81b375ccc72059638
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-18 23:20:16 +0300

    git commit

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserInfo from './UserInfo'; // Import the UserInfo component
+
+function UserProfile() {
+  return <UserInfo />; // Render UserInfo component
+}
+
+export default UserProfile;
