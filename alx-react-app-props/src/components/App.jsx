f4f31c51a12cf52da4dd7b05b3896dffe78beb11
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-18 22:02:05 +0300

    git commit

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ProfilePage from './ProfilePage';
-import { UserContext } from './UserContext';
+import ProfilePage from '../ProfilePage';
+import { UserProvider } from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}>
+    <UserProvider userData={userData}>
       <ProfilePage />
-    </UserContext.Provider>
+    </UserProvider>
   );
 }
 
