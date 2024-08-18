7765c750fa346859765e208ab3bbac7fb91b8869
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-18 22:27:01 +0300

    git commit

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import ProfilePage from './ProfilePage';
 import { UserProvider } from './UserContext';
 
-const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-ReactDOM.render(
-  <UserProvider userData={userData}>
-    <ProfilePage />
-  </UserProvider>,
-  document.getElementById('root')
-);
+  return (
+    <UserProvider userData={userData}>
+      <ProfilePage />
+    </UserProvider>
+  );
+}
+
+export default App;
