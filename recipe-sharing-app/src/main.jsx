b0e500cebd01d37a04c0f2f681de460713b0a413
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-21 00:12:22 +0300

    git commit

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
-import RecipeDetails from './components/RecipeDetails';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
