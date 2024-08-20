c602afed9c25b7e2ae3578a084692487824f7d4b
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-20 18:43:16 +0300

    git commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,15 +1,19 @@
+// src/components/RecipeList.jsx
 import React from 'react';
-import useRecipeStore from './recipeStore'; // Correct import path
+import { Link } from 'react-router-dom';
+import useRecipeStore from '../recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore(state => state.recipes);
 
   return (
     <div>
       {recipes.length > 0 ? (
         recipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <Link to={`/recipes/${recipe.id}`}>
+              <h3>{recipe.title}</h3>
+            </Link>
             <p>{recipe.description}</p>
           </div>
         ))
