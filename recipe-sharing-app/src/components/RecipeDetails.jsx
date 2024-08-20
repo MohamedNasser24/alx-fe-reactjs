8b982cb593545afbd48a24a8f445955495e3c73a
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-20 15:11:02 +0300

    git commit

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import useRecipeStore from '../recipeStore';
+import useRecipeStore from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
