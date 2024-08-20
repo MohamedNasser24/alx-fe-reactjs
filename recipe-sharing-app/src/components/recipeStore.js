f6093c8bbb81373dd2f93a55ff497b2775bb24de
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-21 00:56:08 +0300

    git commit

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,13 +4,12 @@ const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   updateRecipe: (updatedRecipe) => set(state => ({
-    recipes: state.recipes.map(recipe =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    )
+    recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe : recipe)
   })),
   deleteRecipe: (id) => set(state => ({
     recipes: state.recipes.filter(recipe => recipe.id !== id)
-  }))
+  })),
+  setRecipes: (recipes) => set({ recipes })
 }));
 
 export default useRecipeStore;
