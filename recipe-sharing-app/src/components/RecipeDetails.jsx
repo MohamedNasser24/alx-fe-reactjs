a2c20dd0714ec43ad93bc4a156da5011788e7cc7
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-20 13:34:01 +0300

    git commit

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,7 +1,7 @@
 // src/components/RecipeDetails.jsx
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useRecipeStore } from './recipeStore';
+import { useRecipeStore } from '../recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
@@ -24,3 +24,4 @@ const RecipeDetails = () => {
 };
 
 export default RecipeDetails;
+
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,11 @@
 // src/recipeStore.js
 import create from 'zustand';
-import { useRecipeStore } from './recipeStore';
+
 export const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
   deleteRecipe: (id) => set(state => ({
     recipes: state.recipes.filter(recipe => recipe.id !== id)
   })),
