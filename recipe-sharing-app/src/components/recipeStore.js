a28babd2f700c4d8d67e58e43e7d53f85e157ab0
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-21 02:29:25 +0300

    git commit

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -12,7 +12,14 @@ const useRecipeStore = create(set => ({
     recipes: [...state.recipes, newRecipe]
   })),
   setRecipes: (recipes) => set({ recipes }),
-
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+  })),
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
+  })),
   // Actions for managing favorites
   addFavorite: (recipeId) => set(state => ({
     favorites: [...state.favorites, recipeId]
