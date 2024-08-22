b80a55a94298bd5e44439445d6cc268b6e3a8a2c
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-22 21:23:08 +0300

    git commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
