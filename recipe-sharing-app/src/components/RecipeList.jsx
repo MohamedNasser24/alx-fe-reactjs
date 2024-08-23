4b4a45c38f4c9c13d9fda856c9f520ea6fe35369
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-23 23:34:29 +0300

    git commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import useRecipeStore from '../recipeStore';
-import { Link } from 'react-router-dom';
-function RecipeList() {
-  const recipes = useRecipeStore(state => state.recipes);
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = () => {
+  const { recipeId } = useParams();
+  const recipe = useRecipeStore(state => 
+    state.recipes.find(recipe => recipe.id === parseInt(recipeId))
+  );
+
+  if (!recipe) {
+    return <p>Recipe not found.</p>;
+  }
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
-}
+};
 
-export default RecipeList;
+export default RecipeDetails;
+
