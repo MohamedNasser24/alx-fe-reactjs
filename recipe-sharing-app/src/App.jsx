13d7bacb1a478c6b7658635d479c47a107926c13
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-23 23:30:18 +0300

    git commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+
+function App() {
+  return (
+    <div>
+      {/* Main header for the application */}
+      <header>
+        <h1>Recipe Sharing App</h1>
+      </header>
+
+      {/* Navigation menu */}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/add">Add Recipe</Link></li>
+        </ul>
+      </nav>
+
+      {/* Route definitions */}
+      <main>
+        <Routes>
+          {/* Route for the home page displaying the list of recipes */}
+          <Route path="/" element={<RecipeList />} />
+          
+          {/* Route for the add recipe form */}
+          <Route path="/add" element={<AddRecipeForm />} />
+          
+          {/* Route for viewing details of a specific recipe */}
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useRecipeStore from '../recipeStore';
+import { Link } from 'react-router-dom';
+function RecipeList() {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RecipeList;
