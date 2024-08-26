ea43b34c564c67f110d015a89e6ff705b7e73f99
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-26 12:57:17 +0300

    git commit

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormikForm from './components/FormikForm.js';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <FormikForm />
+    </div>
+  );
+};
+
+export default App
