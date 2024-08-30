71add49cc2a89d2cd893f3b98a30cd67403a380d
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-08-31 01:09:14 +0300

    git commit

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 function App() {
   return (
