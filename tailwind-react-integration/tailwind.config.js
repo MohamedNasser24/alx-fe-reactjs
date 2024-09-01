934ee0cd8b43a8ba167762e9521089ed8e197be7
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-02 00:59:37 +0300

    git commit

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,24 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // Paths to all files using Tailwind classes
-    "./public/index.html"          // Include HTML files if used
-  ],
-  darkMode: 'class', // or 'media' to enable dark mode based on user system preferences
-  theme: {
-    extend: {
-      // Add custom theme settings here
-      colors: {
-        customColor: '#ff5722',
-      },
-      spacing: {
-        '72': '18rem',
-        '84': '21rem',
-      },
-    },
-  },
-  plugins: [
-    // Add Tailwind CSS plugins here if needed
-  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
 };
-
