cff2f7e97fdbfd2c2f573a3a1e04e5a69c86bd31
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-02 22:00:06 +0300

    git commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 rounded-lg shadow-lg">
