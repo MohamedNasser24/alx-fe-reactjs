af5256695f0b1e93cae7218d1932e416bf2940fe
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-03 00:45:03 +0300

    git commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,19 +1,18 @@
-
 import React from 'react';
 
 const UserProfile = () => {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:shadow-xl hover:scale-105">
+    <div className="container mx-auto p-4 sm:p-4 md:p-8 lg:p-10 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg">
       <div className="flex flex-col items-center">
         <img
           src="/path/to/profile-image.jpg"
           alt="Profile"
-          className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 lg:w-40 lg:h-40 rounded-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+          className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 lg:w-40 lg:h-40 rounded-full object-cover"
         />
-        <h1 className="text-lg sm:text-lg md:text-xl lg:text-2xl font-bold mt-4 text-blue-800 my-4">
+        <h1 className="text-lg sm:text-lg md:text-xl lg:text-2xl font-bold mt-4">
           John Doe
         </h1>
-        <p className="text-sm sm:text-sm md:text-base lg:text-lg mt-2 text-center text-gray-600">
+        <p className="text-sm sm:text-sm md:text-base lg:text-lg mt-2 text-center">
           A short description about John Doe goes here. It provides a brief overview of the user.
         </p>
       </div>
