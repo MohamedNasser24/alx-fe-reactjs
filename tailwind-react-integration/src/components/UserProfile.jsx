bbfaf2e548896a6d13045e63c6a35d99546dfe7e
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-03 00:37:53 +0300

    git commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,7 +9,7 @@ const UserProfile = () => {
           alt="Profile"
           className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 lg:w-40 lg:h-40 rounded-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
         />
-        <h1 className="text-lg sm:text-lg md:text-xl lg:text-2xl font-bold mt-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
+        <h1 className="text-lg sm:text-lg md:text-xl lg:text-2xl font-bold mt-4 text-blue-800 my-4">
           John Doe
         </h1>
         <p className="text-sm sm:text-sm md:text-base lg:text-lg mt-2 text-center text-gray-600">
@@ -21,6 +21,7 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
 
 
 
