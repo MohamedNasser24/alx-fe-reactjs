63395979624bfe0fc843795d2116def250bef915
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-09 20:06:59 +0300

    git commit

--- recipe-sharing-platform/src/tailwind.config.js
+++ recipe-sharing-platform/src/tailwind.config.js
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}', // Paths to all files where Tailwind CSS classes might be used
-      './public/index.html',         // Include the index.html file in the public directory
-    ],
+    purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false,
     theme: {
       extend: {},
     },
     plugins: [],
-  }
-  
+  };
