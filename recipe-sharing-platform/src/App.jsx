af3c1206a565878b9e6079d776dd6a79e472aed5
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-09 20:19:41 +0300

    git commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,22 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail';
-import AddRecipeForm from './components/AddRecipeForm'; // Import the form
+import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/add-recipe" element={<AddRecipeForm />} /> {/* Add the form route */}
-      </Routes>
-    </Router>
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-500">Welcome to the Recipe Sharing Platform</h1>
+    </div>
   );
 }
 
-export default App
+export default App;
