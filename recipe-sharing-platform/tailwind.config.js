081a2c4219de4ccc2088f29c20d2a3b99fca71e2
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-09 20:39:16 +0300

    git commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Paths to all JavaScript, JSX, TypeScript, and TSX files
-    './public/index.html',         // Include the index.html file in the public directory
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
