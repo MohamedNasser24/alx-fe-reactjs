f6d34f640159def3c135bbd6e5bcdec113232a71
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-13 22:36:35 +0300

    git commit

--- recipe-sharing-platform/src/App.js
+++ recipe-sharing-platform/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail'; // Import RecipeDetail component
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
