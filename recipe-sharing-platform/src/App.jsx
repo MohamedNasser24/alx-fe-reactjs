ac2cadf9668e798d1d7eff6779a6f4621b92cefc
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-14 18:39:12 +0300

    git commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
+  // Dummy function to handle new recipe submission
+  const handleAddRecipe = (recipe) => {
+    console.log('New Recipe Added:', recipe);
+    // Implement your logic to add the recipe to the state or server
+  };
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/add-recipe" element={<AddRecipeForm onAddRecipe={handleAddRecipe} />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
