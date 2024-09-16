086c9ede08226ba0ff8dbd2e4f48fb45e2263afc
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-16 12:20:15 +0300

    git commit

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+        <input type="text" placeholder="Search for GitHub users..." />
+        {/* Add more components here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
