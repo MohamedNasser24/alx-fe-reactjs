c35945e9572c533a74247617c8a6baf2661e3570
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-16 23:52:22 +0300

    git commit

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { fetchUserData } from '../services/githubService';
+import './Search.css';
 
 const Search = () => {
     const [username, setUsername] = useState('');
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const BASE_URL = 'https://api.github.com/users/';
 
 export const fetchUserData = async (username) => {
-    const response = await axios.get(`${BASE_URL}${username}`);
-    return response.data;
+    try {
+        const response = await axios.get(`${BASE_URL}${username}`);
+        return response.data;
+    } catch (error) {
+        throw new Error('User not found');
+    }
 };
