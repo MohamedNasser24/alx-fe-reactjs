6469dc57d0807a7cb06fb42471782d39a5b4d1cb
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-17 00:06:37 +0300

    git commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,16 @@
-// src/services/githubService.js
 import axios from 'axios';
 
+// GitHub API endpoint for advanced search
 const BASE_URL = 'https://api.github.com/search/users';
 
 export const fetchUserData = async (username, location = '', minRepos = '') => {
-    let query = `q=${username}`;
-    if (location) query += `+location:${location}`;
-    if (minRepos) query += `+repos:${minRepos}`;
-
     try {
+        // Construct the query string
+        let query = `q=${username}`;
+        if (location) query += `+location:${location}`;
+        if (minRepos) query += `+repos:>=${minRepos}`;
+
+        // Make the API call
         const response = await axios.get(`${BASE_URL}?${query}`);
         return response.data;
     } catch (error) {
