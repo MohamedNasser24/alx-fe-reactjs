6251e615e3d38d942bee1bbfbb83eb78a0918330
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-19 02:57:10 +0300

    git commit

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
-import { fetchUserData, fetchUsersByQuery } from '../services/githubService';
+import { fetchUsersByQuery } from '../services/githubService';
 
 const Search = () => {
-    const [username, setUsername] = useState('');
-    const [user, setUser] = useState(null);
+    const [query, setQuery] = useState('');
+    const [users, setUsers] = useState([]);
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleSearch = async () => {
+        setLoading(true);
+        setError('');
         try {
-            const userData = await fetchUserData(username);
-            setUser(userData);
-            setError('');
+            const userData = await fetchUsersByQuery(query);
+            setUsers(userData);
         } catch (err) {
             setError(err.message);
-            setUser(null);
+            setUsers([]);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -21,17 +25,23 @@ const Search = () => {
         <div>
             <input
                 type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Enter GitHub username"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Enter search criteria"
             />
             <button onClick={handleSearch}>Search</button>
+
+            {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
-            {user && (
+            {users.length > 0 && (
                 <div>
-                    <h2>{user.login}</h2>
-                    <img src={user.avatar_url} alt={`${user.login}'s avatar`} />
-                    <a href={user.html_url} target="_blank" rel="noopener noreferrer">View Profile</a>
+                    {users.map(user => (
+                        <div key={user.id}>
+                            <h2>{user.login}</h2>
+                            <img src={user.avatar_url} alt={`${user.login}'s avatar`} />
+                            <a href={user.html_url} target="_blank" rel="noopener noreferrer">View Profile</a>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
@@ -39,5 +49,6 @@ const Search = () => {
 };
 
 export default Search;
+
 
 
