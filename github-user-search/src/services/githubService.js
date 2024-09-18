21f4be73fc15475808bba8ad307d5e64e7bd0d51
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-19 02:48:44 +0300

    git commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
+// Function to fetch user data by username
 export const fetchUserData = async (username) => {
     try {
         const response = await axios.get(`${BASE_URL}/users/${username}`);
@@ -11,6 +12,7 @@ export const fetchUserData = async (username) => {
     }
 };
 
+// Function to search for users by query, including location and minimum repos
 export const fetchUsersByQuery = async (query, location = '', minRepos = 0) => {
     try {
         // Construct the query string with additional filters
@@ -21,22 +23,11 @@ export const fetchUsersByQuery = async (query, location = '', minRepos = 0) => {
         if (minRepos) {
             fullQuery += ` repos:>${minRepos}`;
         }
-        const handleSearch = async (e) => {
-            e.preventDefault();
-            setError('');
-            try {
-                const data = await fetchUsersByQuery(query, location, minRepos);
-                setUsers(data);
-            } catch (err) {
-                setError('Looks like we can’t find any users');
-                setUsers([]); // Clear previous results on error
-            }
-        };
-        
+
+        // API endpoint for searching users
         const response = await axios.get(`${BASE_URL}/search/users?q=${encodeURIComponent(fullQuery)}`);
         return response.data.items; // Return the array of user items
     } catch (error) {
         throw new Error(error.response ? error.response.data.message : 'Error fetching data');
     }
 };
-
