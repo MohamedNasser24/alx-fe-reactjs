94b3d72df4aa2836a7946e45b0f112d2a3d2765b
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-21 01:03:48 +0300

    git commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
+// Function to fetch user data from GitHub API
 export const fetchUserData = async (username) => {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
+    try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        return response.data; // Return the user data
+    } catch (error) {
+        throw error; // Rethrow error for handling in the component
+    }
 };
