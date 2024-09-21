549d6644f3c6c708d9907719d2f762ef11661353
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-21 16:31:36 +0300

    git commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,11 +2,12 @@
 
 import axios from 'axios';
 
-export const fetchUserData = async (username, location, minRepos) => {
-  const query = `type:user+in:login+${username}` +
-                (location ? `+location:${location}` : '') +
-                (minRepos ? `+repos:>=${minRepos}` : '');
-  
-  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
-  return response.data;
+export const fetchUserData = async (username, location = '', minRepos = '') => {
+    const query = `type:user in:login ${username}` +
+                  (location ? ` location:${location}` : '') +
+                  (minRepos ? ` repos:>=${minRepos}` : '');
+
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+    return response.data;
 };
+
