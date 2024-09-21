943c4293141f4c81b3ca7647ee1eb3f614696f40
mohamed mahmoud <Mohamednasser9354@gmail.com>
2024-09-21 16:38:43 +0300

    git commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,15 @@
+
 import React from 'react';
 import Search from './components/Search';
 
 const App = () => {
     return (
         <div>
-            <h1>GitHub User Search</h1>
+            <h1 className="text-center text-2xl font-bold mt-4">GitHub User Search</h1>
             <Search />
         </div>
     );
 };
 
 export default App;
+
